3c7b5249ce7822a56c56638f25bcc423c39e3606
Nur <nuraalamp@gmail.com>
2021-12-03 12:47:39 +0000

    added

--- projo/urls.py
+++ projo/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, path, include
 from django.contrib import admin
-from django.urls import path, include
+# from django.urls import path, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', 'blog.urls'),
+    url(r'^blog/', include('blog.urls'))),
 ]
